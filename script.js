03c0d9eb51d3ce65f65f3c74ce5ef853db4f3263
Pragati Mishra <144036589+Pragati-cloud@users.noreply.github.com>
2024-11-24 17:25:29 +0530

    Add files via upload
    Leads Tracker - Save and Organize Your Links Effortlessly
    
    Leads Tracker is a lightweight Chrome extension that helps you save important links and your current tab with ease. Perfect for staying organized and keeping track of valuable resources, it offers a user-friendly interface for quick access to all your saved URLs.
    
    Features:
    
    Save your current tab or any link with one click.
    Access and manage saved links directly from the extension.
    Simple, clean, and intuitive design.
    Whether you're a student, professional, or casual browser, LinkSaver makes managing bookmarks hassle-free.
    
    Getting Started:
    
    Clone the repository.
    Load the unpacked extension in Chrome.
    Start saving your favorite links!
    Contributions are welcome! Feel free to submit issues or pull requests.

--- script.js
+++ script.js
@@ -1,95 +1,83 @@
+/*let country = ["India","usa", "indonesia","monaco","tuvalu"]
 
-let cardarray = [];
-let sum =0;
-let hasblackjack = false;
-let isAlive = false;
-let message ="";
-let displaymsg = document.getElementById("message");
+console.log(country);
+country.push("pakistan");
+console.log(country);
 
-let total = document.getElementById("sum");
-let cards= document.getElementById("cards");
-let newcard= document.getElementById("new");
-
-let player ={
-   name: "Pragati",
-   points : 142
-}
-
-document.getElementById("player").textContent = player.name +": $" + player.points;
+country.pop()
+console.log(country);
 
 
-function start(){
-    let firstCard = getRandomCard();
-let secondCard =getRandomCard();
-cardarray=[firstCard, secondCard];
-let sum = firstCard + secondCard;
-isAlive = true;
-}
-function StartGame(){
-    renderGame();
-}
+let hands =["rocks", "paper","scissor"];
 
-function renderGame(){
-
-    cards.textContent = "Cards: "
-    for(i =0; i<cardarray.length; i++){
-        cards.textContent += cardarray[i] +" ";
-    }
-
-    total.textContent = "Sum:"+ " " + sum; 
+function getHand(){
+    let ram = Math.floor(Math.random() *3);
+    return hands[ram];
     
-    if(sum === 21){
-        message = "Wohoo !! You Won Congratulations";
-        hasblackjack = true;
-    }else if(sum < 21){
-        message = "Do you want to draw a new card?";
-    
-    }else{
-        message = "You're out of the game!";
-        isAlive= false;
-    }
-    
-    displaymsg.textContent = message;
-    console.log(hasblackjack);
-    console.log(message);
 }
-
-function newc(){
-    
-    console.log("Drawing a new card from the deck!");
-    if(sum <=21){
-    let c = getRandomCard();
-    sum +=c; 
-    cardarray.push(c);
-    console.log(cardarray);
-    renderGame();
-    }
-}
-
-function getRandomCard(){
-    num = Math.floor(Math.random() * 13) + 1;
-    if(num ===1) return 11;
-    else if(num>=11 ) return 10;
-    else return num;
-}
-/*let age=22;
-
-if(age >=21){
-    console.log("Welcome");
-}else{
-    console.log("not eligible");
-}
-  
-#array
-
-arr= [20, "Pragati", true]
-
-###array 
-let sen = ["hello", "my", "name", "is", "Pragati"];
-let greeting = document.getElementById("greet");
-
-for(i=0; i<=sen.length; i++){
-    greeting.textContent += sen[i] + " ";
-}
-
+console.log(getHand())
 */
+
+let arr =[]
+let input = document.getElementById("input");
+
+let btn= document.getElementById("input-btn");
+const ul = document.getElementById("UL");
+let deletebtn= document.getElementById("delete-btn")
+let savetab= document.getElementById("save-btn")
+
+const ls = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(ls)
+
+//truthy
+if(ls){
+    arr= ls;
+    render()
+}
+
+const tabs =[
+    {url: "www.google.com"}
+]
+
+savetab.addEventListener("click" , function(){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        arr.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(arr))
+        render(arr)
+    })
+     console.log(tabs)
+})
+
+deletebtn.addEventListener("click", function(){
+    console.log("Delete")
+    localStorage.clear();
+    arr=[]
+    render()
+})
+
+btn.addEventListener("click", function() {
+    console.log("button")
+    arr.push(input.value);
+    input.value=null
+
+    console.log(arr)
+
+    //saving array to localstorage
+    localStorage.setItem("myLeads", JSON.stringify(arr))
+
+    render()
+    console.log(localStorage.getItem("myLeads"))
+})
+function render(){
+let listitem =""
+for(i=0;i<arr.length;i++){
+    console.log(arr[i]);
+    listitem += `
+    <li>
+        <a target='_blank' href='${arr[i]}'>
+        ${arr[i]}
+    </li>` ;
+}
+ul.innerHTML =listitem
+}
